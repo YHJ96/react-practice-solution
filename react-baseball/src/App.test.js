7f80f7cbc8adf45b6eac766e0177a053f92cf78f
YHJ96 <yhj878812@naver.com>
2022-07-17 15:50:26 +0900

    feat: confirm 목 함수 추가 완료

--- react-baseball/src/App.test.js
+++ react-baseball/src/App.test.js
@@ -10,7 +10,27 @@ describe("✅ input 테스트 케이스", () => {
         const inputValue = screen.getByDisplayValue("193");
         expect(inputValue.value).toEqual("193");
     });
-    // test("잘못된 값을 입력하는 경우 alert 경고창으로 나타내야합니다.", () => {});
+    test("문자가 들어올 경우 alert 경고창으로 나타내야합니다.", () => {
+        const alertMock = jest.spyOn(window, "alert");
+        const { container } = render(<App/>);
+        const $input = container.querySelector("input");
+        fireEvent.change($input, { target: { value: "123a" } });
+        expect(alertMock).toHaveBeenCalledTimes(1);
+    });
+    test("중복된 숫자가 들어올 경우 alert 경고창으로 나타내야합니다.", () => {
+        const alertMock = jest.spyOn(window, "alert");
+        const { container } = render(<App/>);
+        const $input = container.querySelector("input");
+        fireEvent.change($input, { target: { value: "122" } });
+        expect(alertMock).toHaveBeenCalledTimes(1);
+    });
+    test("4자리이상 숫자가 들어올 경우 alert 경고창으로 나타내야합니다.", () => {
+        const alertMock = jest.spyOn(window, "alert");
+        const { container } = render(<App/>);
+        const $input = container.querySelector("input");
+        fireEvent.change($input, { target: { value: "1234" } });
+        expect(alertMock).toHaveBeenCalledTimes(1);
+    });
 });
 
 describe("⚾️ 아구 게임 테스트 케이스", () => {
@@ -76,6 +96,19 @@ describe("🎊 승리 테스트 케이스", () => {
         expect($result.textContent).toEqual("승리");
         const $resetbutton = screen.getByText("재시작");
         expect($resetbutton).toBeInTheDocument();
+    });
+    test("재시작 버튼을 눌렀을 경우 window.confirm창을 나타내야합니다.", () => {
+        const confirmMock = jest.spyOn(window, "confirm");
+        const { container } = render(<App />);
+        const $input = container.querySelector("input");
+        const $button = container.querySelector("button");
+        const $result = container.querySelector("#result");
+        fireEvent.change($input, { target: { value: "123" } });
+        fireEvent.click($button);
+        expect($result.textContent).toEqual("승리");
+        const $resetbutton = screen.getByText("재시작");
+        fireEvent.click($resetbutton);
+        expect(confirmMock).toHaveBeenCalledTimes(1);
     });
     // test("재시작 버튼을 눌렀을 경우 새로고침이 되어야 합니다.", () => {});
 });
