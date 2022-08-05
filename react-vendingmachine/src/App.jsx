29a946ff65b7e2db7245ad70d3af8485446bc0b8
YHJ96 <yhj878812@naver.com>
2022-08-05 22:45:39 +0900

    feat: 목업 생성 완료

--- react-vendingmachine/src/App.jsx
+++ react-vendingmachine/src/App.jsx
@@ -1,22 +1,30 @@
-/* 아래의 주석은 지우시면 안됩니다. */
-/* global MissionUtils */
-
-import React from 'react';
+import React, { useState } from 'react';
 // SPA 구성 라이브러리
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-/* API 호출 상수 */
-const Random = MissionUtils.Random;
+// import { Routes, Route, Link } from 'react-router-dom';
+import Management from './pages/Management';
+import Charge from './pages/Charge';
+import Product from './pages/Product';
 
 function App() {
+  // products의 요소 데이터 형식 { product: "솔의눈", price: "1000", count: "1" }
+  const [products, setProducts] = useState([]);
+  // 잔돈의 데이터 형식입니다.
+  const [coins, setCoins] = useState({ 500: 0, 100: 0, 50: 0, 10: 0 });
+  /* API 호출 상수 */
+  // const Random = global.MissionUtils.Random;
+  // const randomNumber = Random.pickNumberInRange(1, 10);
   /* API 예시 입니다. 확인하시고 지우시면 됩니다. */
-  const randomNumber = Random.pickNumberInRange(1, 10);
-  console.log(randomNumber);
-  /* react-router-dom으로 SPA를 App애 먼저 구성해주세요. */
+  // console.log(randomNumber);
+  /* react-router-dom으로 SPA를 먼저 구성해주세요. */
   return (
-    <div>
-      <div id="app">처음부터 구성해보세요.</div>
-    </div>
+    <React.Fragment>
+        <button id='management-button'>상품 관리</button>
+        <button id='charge-button'>잔돈 충전</button>
+        <button id='product-button'>상품 구매</button>
+        <Management products={products} setProducts={setProducts}/>
+        <Charge coins={coins} setCoins={setCoins}/>
+        <Product products={products} setProducts={setProducts} coins={coins} setCoins={setCoins}/>
+    </React.Fragment>
   );
 }
 
